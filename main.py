9d8a775486c1cfb34da8e45f7d81c5fc3029c3ce
zhangzhiyuan <zhangzhiyuan@swellai.net>
2025-02-06 01:23:23 +0800

    Feat: add read batch table

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from src.db.init_db import init_db
 def main():
     init_db()
     readCaseList('data/test.xls')
+    readCaseExclusiveAndBatch('data/人大案例第一批.xlsx', '中国人民大学', 1)
+    readCaseExclusiveAndBatch('data/人大案例第二批.xlsx', '中国人民大学', 2)
+    m, n = readCaseExclusiveAndBatch('data/浙大案例第一批.xlsx', '浙江大学', 1)
+    p, q = readCaseExclusiveAndBatch('data/浙大案例第二批.xlsx', '浙江大学', 2)
+    for case in n + q:
+        print(case)
     # readCaseList('data/testCaseList.xlsx')
-    readBrowsingAndDownloadRecord_Tsinghua('data/testTsinghua.xls')
+    # readBrowsingAndDownloadRecord_Tsinghua('data/testTsinghua.xls')
     # readBrowsingAndDownloadRecord_Tsinghua('data/testTsinghua.xlsx')
-    readBrowsingAndDownloadData_HuaTu('data/testHuaTu.xls', 2024)
+    # readBrowsingAndDownloadData_HuaTu('data/testHuaTu.xls', 2024)
     # readBrowsingAndDownloadData_HuaTu('data/testHuaTu.xls', 2023)
     # readBrowsingAndDownloadData_HuaTu('data/testHuaTu.xls', 2022)
     # exportCaseList('data/testCL.xlsx')
